607d9fb0efbdfa9b480ebf0dfae96cd747611859
zopalmer14 <zopalmer14@gmail.com>
2024-02-09 18:44:56 -0500

    create function to check tic-tac-toe board for player victory

--- dynamic.js
+++ dynamic.js
@@ -37,11 +37,79 @@ const gameController = function gameController() {
         };
 
         const displayBoard = () => {
-            console.table(game_board)
+            console.table(game_board);
         }
     
         return { getBoard, updateBoard, displayBoard };
     }();
 
-    return { gameBoard };
+    // create a function to check whether a player has won 
+    // returns true if so, false otherwise
+    const checkVictory = function checkVictory(player) {
+        const board_state = gameBoard.getBoard();
+        let victory_status = false;
+        
+        // check vertical win possibilities
+        for (let col = 0; col < 3; col++) {
+            let num_matches = 0;
+            for (let row = 0; row < 3; row++) {
+                if (board_state[row][col] === player.getSymbol()) {
+                    num_matches++;
+                } else {
+                    break;
+                }
+            }
+            if (num_matches === 3) {
+                victory_status = true;
+                break;
+            } 
+        }
+
+        // check horizontal win possibilities
+        // we use map to test each row individually for whether every value in it equals the player's symbol
+        const symbol_check = (val) => val === player.getSymbol();
+        const triple_match = board_state.map((row) => row.every(symbol_check));
+        // if any row returns true then the player has won
+        if (triple_match.some(Boolean)) {
+            victory_status = true;
+        }
+
+        // check diagonal win possibilities
+        // left to right
+        let num_matches = 0;
+        for (let row = 0; row < 3; row++) {
+            let col = row;
+            
+            if (board_state[row][col] === player.getSymbol()) {
+                num_matches++;
+            } else {
+                break;
+            }
+        }
+        // check number of matches
+        if (num_matches === 3) {
+            victory_status = true;
+        } 
+
+        // right to left
+        // reset the number of matches 
+        num_matches = 0;
+        for (let row = 0; row < 3; row++) {
+            let col = 2 - row;
+            
+            if (board_state[row][col] === player.getSymbol()) {
+                num_matches++;
+            } else {
+                break;
+            }
+        }
+        // check number of matches
+        if (num_matches === 3) {
+            victory_status = true;
+        } 
+
+        return victory_status;
+    }
+
+    return { gameBoard, getActivePlayer, switchTurn, checkVictory };
 }();
